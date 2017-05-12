394cfe742d08625c70ba27ded369db7c7cc889b2
Derya Özgün Yalçın <d.yalcin@emakina.nl>
2017-05-12 15:24:18 +0300

    multiple clicks

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@
 
 function click(e) {
   chrome.tabs.executeScript(null,
-      {code:"function changeit() { var caesarShift=function(r,a){if(0>a)return caesarShift(r,a+26);for(var t='',e=0;e<r.length;e++){var f=r[e];if(f.match(/[a-z]/i)){var i=r.charCodeAt(e);65>i||i>90?97>i||i>122||(f=String.fromCharCode((i-97+a)%26+97)):f=String.fromCharCode((i-65+a)%26+65)}t+=f}return t}; for(var divs=document.getElementsByClassName('noselect'),i=0;i<divs.length;i++){ divs[i].innerHTML=caesarShift(divs[i].innerHTML,25);divs[i].style.color='black';divs[i].style.textShadow='none'; }} changeit(); "});
+      {code:"function changeit() { var caesarShift=function(r,a){if(0>a)return caesarShift(r,a+26);for(var t='',e=0;e<r.length;e++){var f=r[e];if(f.match(/[a-z]/i)){var i=r.charCodeAt(e);65>i||i>90?97>i||i>122||(f=String.fromCharCode((i-97+a)%26+97)):f=String.fromCharCode((i-65+a)%26+65)}t+=f}return t}; for(var divs=document.getElementsByClassName('noselect'),i=0;i<divs.length;i++){ if(!divs[i].classList.contains('retrified')){ divs[i].innerHTML=caesarShift(divs[i].innerHTML,25);divs[i].style.color='black';divs[i].style.textShadow='none'; divs[i].classList.add('retrified'); }}} changeit(); "});
   window.close();
 }
 
